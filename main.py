c02dc7a9748982abd2a66404c9473023c3eb207a
fjmorais <fabiano.morais@outlook.com>
2024-02-19 20:20:01 -0300

    Voltando com o meu arquivo main

---
+++ main.py
@@ -0,0 +1,25 @@
+print ("Jornada de Dados")
+
+# variáveis
+
+idade = 42
+nome = "Fabiano"
+
+#print
+
+print(idade)
+
+#soma
+
+nova_idade = idade + 1
+print (nova_idade)
+
+#lista
+
+lista = [1,2,3,4,5,6]
+
+#Métodos
+
+lista_invertida = lista.reverse()
+
+print(lista_invertida)
